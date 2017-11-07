a23e70add07c7138c671f6c0b4ee43bb8d050504
scarletttap <33457269+scarletttap@users.noreply.github.com>
2017-11-07 09:43:10 -0500

    First upload

---
+++ Personality_ST.py
@@ -0,0 +1,21 @@
+name = "Scarlett"
+state = "California"
+city = "New York City"
+tvshow = "How I Met Your Mother "
+shop = "Urban Outfitters "
+sister = "bettina"
+season = "fall"
+drink  = "chai lattes"
+
+print (name + " likes to watch " + tvshow + "while visiting " + state)
+print ("if you ask me, I think she would rather go to " + shop + "in " + city)
+
+
+print ("what's your favorite subject?")
+subject = input()
+
+if subject == "English" :
+    print ("thats my favorite too!")
+else:
+    print (subject + " is a pretty good class too.")
+    
---
+++ mad libs_ Scarlett.py
@@ -0,0 +1,74 @@
+import time
+
+### MAD LIBS ###
+### http://cdn.rainbowresource.netdna-cdn.com/products/032883i1.jpg
+
+print("Give me a foreign country")
+country1 = input ()
+
+print ("Give me an adverb")
+
+adverb1= input ()
+
+print ("Give me an adjective")
+
+adjective1 = input ()
+
+print ("Give me an animal")
+
+animal1 = input ()
+
+print ("Give me a verb ending in ing")
+
+verbing1 = input ()
+
+print ("Give me a verb")
+
+verb1 = input ()
+
+print ("Give me a verb ending in ing")
+
+verbing2 = input ()
+
+print ("Give me an adverb")
+
+adverb2 = input ()
+
+print ("Give me an adjective")
+
+adjective2 = input ()
+
+print (" Give me a place")
+
+place1 = input ()
+
+print ("Give me a type of liquid")
+
+liquid1 = input ()
+
+print ("Give me a body part")
+
+body1 = input ()
+
+print ("Give me a verb")
+
+verb2 = input ()
+
+
+
+
+### Begin Mad Lib ###
+
+print ("if you are traveling in " + country1 +" and find yourself having to cross a piranha-filled river, here is how to do it " + adverb1)
+
+print ("Piranhas are more " + adjective1 + " during the day, so cross the river at night.")
+
+print (" Avoid areas with netted " + animal1 + " traps- piranhas may be " + verbing1 + " there looking to " + verb1 + " them!")
+
+print ("When " + verbing2 + " in the river, swim " + adverb2 + " You don't want to wake them up and make them " + adjective2)
+
+print (" Whatever you do, if you have an open wound, try to find another way to get back to the " + place1)
+
+print (" Piranhas are attracted to fresh " + liquid1 + " and will most likley take a bite out of your " + body1 + " if you " + verb2 + " in the water ")
+
+time.sleep (60)
